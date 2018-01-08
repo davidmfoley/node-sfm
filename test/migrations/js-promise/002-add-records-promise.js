b592545757e8657deb5974eba52ebb8420d29c9a
Dave Foley <davidmfoley@gmail.com>
2018-01-08 11:38:39 -0800

    Support selecting records with promise/async

--- test/migrations/js-promise/002-add-records-promise.js
+++ test/migrations/js-promise/002-add-records-promise.js
@@ -1,4 +1,6 @@
 module.exports = async function(client) {
   await client.query("insert into foobar(id, name) values(1, 'one');");
   await client.query("insert into foobar(id, name) values($1, $2);", [2, 'two']);
+  let { rows } = await client.query('select * from foobar');
+  if (rows.length !== 2) throw new Error('could not get added rows');
 };
